eb94be284ab48bf4a3e45cf3128736e25b07afb6
J-Ciro <jciro.1984@gmail.com>
2025-01-20 13:58:50 -0500

    chore(src): fixed issue with merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-<<<<<<< HEAD
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -13,14 +12,4 @@ import { FooterComponent } from './components/footer/footer.component';
 })
 export class AppComponent {
 
-=======
-
-@Component({
-  selector: 'app-root',
-  imports: [HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-})
-export class AppComponent {
->>>>>>> 1bdb79270d4894d0982ab3287b881b3ae77b3143
 }
