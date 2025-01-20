fd6d81731773253e9ee5767f5c73c64dc627e1ed
J-Ciro <jciro.1984@gmail.com>
2025-01-20 14:02:51 -0500

    fix(src): issue with the original repo files and style files that made the app crash

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,26 +14,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<< HEAD
   it(`should have the 'eco-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-=======
-  it(`should have the 'sketch-angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('sketch-angular');
->>>>>>> 1bdb79270d4894d0982ab3287b881b3ae77b3143
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-<<<<<<< HEAD
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, eco-angular');
-=======
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sketch-angular');
->>>>>>> 1bdb79270d4894d0982ab3287b881b3ae77b3143
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,5 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-<<<<<<< HEAD
 
   
-=======
->>>>>>> 1bdb79270d4894d0982ab3287b881b3ae77b3143
